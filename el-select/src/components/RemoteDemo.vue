4b2ccf9a4dcc3dd90033c05b6c17c7ca3dd9907c
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2021-04-10 12:45:31 +0800

    Update RemoteDemo.vue

--- el-select/src/components/RemoteDemo.vue
+++ el-select/src/components/RemoteDemo.vue
@@ -7,68 +7,133 @@
     reserve-keyword
     placeholder="请输入关键词"
     :remote-method="remoteMethod"
-    :loading="loading">
+    :loading="loading"
+  >
     <el-option
       v-for="item in options"
       :key="item.value"
       :label="item.label"
-      :value="item.value">
+      :value="item.value"
+    >
     </el-option>
   </el-select>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        options: [],
-        value: [],
-        list: [],
-        loading: false,
-        states: ["Alabama", "Alaska", "Arizona",
-        "Arkansas", "California", "Colorado",
-        "Connecticut", "Delaware", "Florida",
-        "Georgia", "Hawaii", "Idaho", "Illinois",
-        "Indiana", "Iowa", "Kansas", "Kentucky",
-        "Louisiana", "Maine", "Maryland",
-        "Massachusetts", "Michigan", "Minnesota",
-        "Mississippi", "Missouri", "Montana",
-        "Nebraska", "Nevada", "New Hampshire",
-        "New Jersey", "New Mexico", "New York",
-        "North Carolina", "North Dakota", "Ohio",
-        "Oklahoma", "Oregon", "Pennsylvania",
-        "Rhode Island", "South Carolina",
-        "South Dakota", "Tennessee", "Texas",
-        "Utah", "Vermont", "Virginia",
-        "Washington", "West Virginia", "Wisconsin",
-        "Wyoming","北京","天津","上海","重庆","河北","山西",
-        "辽宁","吉林","黑龙江","江苏","浙江","安徽","福建","江西",
-        "山东","河南","湖北","湖南","广东","海南","四川","贵州","云南",
-        "陕西","甘肃","青海","内蒙古","广西","西藏","宁夏","新疆维吾尔自治区",
-        "香港","澳门","台湾"
-        ]
+export default {
+  data() {
+    return {
+      options: [],
+      value: [],
+      list: [],
+      loading: false,
+      states: [
+        "Alabama",
+        "Alaska",
+        "Arizona",
+        "Arkansas",
+        "California",
+        "Colorado",
+        "Connecticut",
+        "Delaware",
+        "Florida",
+        "Georgia",
+        "Hawaii",
+        "Idaho",
+        "Illinois",
+        "Indiana",
+        "Iowa",
+        "Kansas",
+        "Kentucky",
+        "Louisiana",
+        "Maine",
+        "Maryland",
+        "Massachusetts",
+        "Michigan",
+        "Minnesota",
+        "Mississippi",
+        "Missouri",
+        "Montana",
+        "Nebraska",
+        "Nevada",
+        "New Hampshire",
+        "New Jersey",
+        "New Mexico",
+        "New York",
+        "North Carolina",
+        "North Dakota",
+        "Ohio",
+        "Oklahoma",
+        "Oregon",
+        "Pennsylvania",
+        "Rhode Island",
+        "South Carolina",
+        "South Dakota",
+        "Tennessee",
+        "Texas",
+        "Utah",
+        "Vermont",
+        "Virginia",
+        "Washington",
+        "West Virginia",
+        "Wisconsin",
+        "Wyoming",
+        "北京",
+        "天津",
+        "上海",
+        "重庆",
+        "河北",
+        "山西",
+        "辽宁",
+        "吉林",
+        "黑龙江",
+        "江苏",
+        "浙江",
+        "安徽",
+        "福建",
+        "江西",
+        "山东",
+        "河南",
+        "湖北",
+        "湖南",
+        "广东",
+        "海南",
+        "四川",
+        "贵州",
+        "云南",
+        "陕西",
+        "甘肃",
+        "青海",
+        "内蒙古",
+        "广西",
+        "西藏",
+        "宁夏",
+        "新疆维吾尔自治区",
+        "香港",
+        "澳门",
+        "台湾",
+      ],
+    };
+  },
+  mounted() {
+    this.list = this.states.map((item) => {
+      return { value: `value:${item}`, label: `${item}` };
+    });
+  },
+  methods: {
+    remoteMethod(query) {
+      if (query !== "") {
+        this.loading = true;
+        setTimeout(() => {
+          this.loading = false;
+          this.options = this.list.filter((item) => {
+            return item.label.toLowerCase().indexOf(query.toLowerCase()) > -1;
+          });
+        }, 200);
+      } else {
+        this.options = [];
       }
     },
-    mounted() {
-      this.list = this.states.map(item => {
-        return { value: `value:${item}`, label: `${item}` };
-      });
-    },
-    methods: {
-      remoteMethod(query) {
-        if (query !== '') {
-          this.loading = true;
-          setTimeout(() => {
-            this.loading = false;
-            this.options = this.list.filter(item => {
-              return item.label.toLowerCase()
-                .indexOf(query.toLowerCase()) > -1;
-            });
-          }, 200);
-        } else {
-          this.options = [];
-        }
-      }
-    }
-  }
+  },
+};
 </script>

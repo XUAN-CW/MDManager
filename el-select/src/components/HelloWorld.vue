ed40a7b8d65a066c6066f402524d283f44a1c325
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2021-04-10 11:00:59 +0800

    想要的效果 - 本地

--- el-select/src/components/HelloWorld.vue
+++ el-select/src/components/HelloWorld.vue
@@ -1,25 +1,22 @@
 <template>
   <div>
-    <el-form ref="form" :model="form" >
-      <el-form-item label="知识点">
-        <el-select 
-          v-model="form.knowledge"
-          clearable
-          multiple
-          filterable
-          allow-create
-          default-first-option
-          placeholder="输入知识点内容，用回车添加"
-          size="100"
-        >
-          <el-option
-            v-for="item in knowledge"
-            :key="item.value"
-            :label="item.label"
-            :value="item.label"
-          ></el-option>
-        </el-select>
-      </el-form-item>
+    <el-form ref="form" :model="form">
+      <el-select
+        v-model="value"
+        multiple
+        filterable
+        allow-create
+        default-first-option
+        placeholder="输入知识点内容，用回车添加"
+        size="100"
+      >
+        <el-option
+          v-for="item in list"
+          :key="item.value"
+          :label="item.label"
+          :value="item.label"
+        ></el-option>
+      </el-select>
     </el-form>
   </div>
 </template>
@@ -28,13 +25,118 @@
 export default {
   data() {
     return {
-      knowledge: [
-        { label: "知识点1" },
-        { label: "知识点2" },
-        { label: "知识点3" },
+
+      options: [],
+      value: [],
+      list: [],
+      loading: false,
+      states: [
+        "Alabama",
+        "Alaska",
+        "Arizona",
+        "Arkansas",
+        "California",
+        "Colorado",
+        "Connecticut",
+        "Delaware",
+        "Florida",
+        "Georgia",
+        "Hawaii",
+        "Idaho",
+        "Illinois",
+        "Indiana",
+        "Iowa",
+        "Kansas",
+        "Kentucky",
+        "Louisiana",
+        "Maine",
+        "Maryland",
+        "Massachusetts",
+        "Michigan",
+        "Minnesota",
+        "Mississippi",
+        "Missouri",
+        "Montana",
+        "Nebraska",
+        "Nevada",
+        "New Hampshire",
+        "New Jersey",
+        "New Mexico",
+        "New York",
+        "North Carolina",
+        "North Dakota",
+        "Ohio",
+        "Oklahoma",
+        "Oregon",
+        "Pennsylvania",
+        "Rhode Island",
+        "South Carolina",
+        "South Dakota",
+        "Tennessee",
+        "Texas",
+        "Utah",
+        "Vermont",
+        "Virginia",
+        "Washington",
+        "West Virginia",
+        "Wisconsin",
+        "Wyoming",
+        "北京",
+        "天津",
+        "上海",
+        "重庆",
+        "河北",
+        "山西",
+        "辽宁",
+        "吉林",
+        "黑龙江",
+        "江苏",
+        "浙江",
+        "安徽",
+        "福建",
+        "江西",
+        "山东",
+        "河南",
+        "湖北",
+        "湖南",
+        "广东",
+        "海南",
+        "四川",
+        "贵州",
+        "云南",
+        "陕西",
+        "甘肃",
+        "青海",
+        "内蒙古",
+        "广西",
+        "西藏",
+        "宁夏",
+        "新疆维吾尔自治区",
+        "香港",
+        "澳门",
+        "台湾",
       ],
-      form: {},
     };
+  },
+  mounted() {
+    this.list = this.states.map((item) => {
+      return { value: `value:${item}`, label: `${item}` };
+    });
+  },
+  methods: {
+    remoteMethod(query) {
+      if (query !== "") {
+        this.loading = true;
+        setTimeout(() => {
+          this.loading = false;
+          this.options = this.list.filter((item) => {
+            return item.label.toLowerCase().indexOf(query.toLowerCase()) > -1;
+          });
+        }, 200);
+      } else {
+        this.options = [];
+      }
+    },
   },
 };
 </script>

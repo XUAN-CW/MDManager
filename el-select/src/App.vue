7532904e819fe5ca23f24f17c460e2ce2869748f
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2021-04-10 10:29:24 +0800

    RemoteDemo

--- el-select/src/App.vue
+++ el-select/src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <RemoteDemo/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import RemoteDemo from './components/RemoteDemo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    RemoteDemo
   }
 }
 </script>

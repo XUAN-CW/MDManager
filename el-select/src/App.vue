b472f13aa498c696c7d3ea8bccea0febe83ce07c
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2021-04-10 12:45:34 +0800

    Update App.vue

--- el-select/src/App.vue
+++ el-select/src/App.vue
@@ -1,20 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <RemoteDemo/>
+    <TablesDemo/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import RemoteDemo from './components/RemoteDemo.vue'
+import TablesDemo from './components/TablesDemo.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    RemoteDemo
+    TablesDemo
   }
 }
 </script>
